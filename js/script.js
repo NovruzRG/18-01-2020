242fadbe604d0933a2f204ea85fd138fff3a18c0
NovruzRG <NovruzRG@code.local>
2020-01-18 14:46:22 +0400

    js first

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// console.log("Hello world!!")
+// alert("Hello world!!")
+// var username = "Novruz"
+
+// if(username=="Novruz"){
+//     console.log(username)
+// }else{
+//     console.log("not")
+// }
+
+// console.log(typeof username)
+
+
+var season = prompt("Ayin nomresini daxil edin");
+switch (season){
+case "1":
+console.log("Yanvar");
+break;
+case "2":
+console.log("Fevral");
+break;
+case "3":
+console.log("Mart");
+break;
+case "4":
+console.log("Aprel");
+break;
+case "5":
+console.log("May");
+break;
+case "6":
+console.log("Iyun");
+break;
+case "7":
+console.log("Iyul");
+break;
+case "8":
+console.log("Avqust");
+break;
+case "9":
+console.log("Sentyabr");
+break;
+case "10":
+console.log("Oktyabr");
+break;
+case "11":
+console.log("Noyabr");
+break;
+case "12":
+console.log("Dekabr");
+break;
+}
+

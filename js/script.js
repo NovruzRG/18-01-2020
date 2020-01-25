b249ee546d45ee54dde41b9bdce3afa0ad05d501
NovruzRG <NovruzRG@code.local>
2020-01-25 14:43:36 +0400

    25-01-2020

--- js/script.js
+++ js/script.js
@@ -92,17 +92,80 @@
 // gettime(totalMinutes);
 
 
-let message = prompt("sozu yazin")
+// let message = prompt("sozu yazin")
 
-function checkPolidrome(message){
-    let j = message.length -1;
-    for (let i =0; i<message.length; i++){
-        if(message[i]!=message[j]){
-            console.log("Not Polidrome");
-            return;
+// function checkPolidrome(message){
+//     let j = message.length -1;
+//     for (let i =0; i<message.length; i++){
+//         if(message[i]!=message[j]){
+//             console.log("Not Polidrome");
+//             return;
+//         }
+//         j--
+//     }
+//    console.log("This is Polidrome")
+// }
+// checkPolidrome(message)
+
+
+// let arr = [2, 4, 7, 3, 5];
+
+// let maxArr = arr[0]
+
+// function findMax() {
+//     for (i = 0; i < arr.length; i++) {
+
+//         if (maxArr < arr[i]) {
+//             maxArr = arr[i];
+
+//         }
+//     }
+//     console.log(maxArr)
+// }
+// findMax(arr)
+
+// let minArr = arr[0]
+
+// function findMin() {
+//     for (i = 0; i < arr.length; i++) {
+
+//         if (minArr > arr[i]) {
+//             minArr = arr[i];
+//         }
+//     }
+//     console.log(minArr)
+// }
+// findMin(arr)
+
+
+let arry = [2, 4, 7, 3, 5, 2, 6];
+let arry2 = [];
+
+function findmod2() {
+    for (i = 0; i < arry.length; i++) {
+
+        if (arry[i] % 2 == 0 && arry2.indexOf(arry[i]) < 0) {
+
+            arry2.push(arry[i]);
         }
-        j--
     }
-   console.log("This is Polidrome")
+    console.log(arry2)
 }
-checkPolidrome(message)
+findmod2(arry)
+
+let arrystr = ["a", "b", "a", "c", "b", "b", "a", "c", "b"];
+sortarrystr = arrystr.sort();
+let tempData = sortarrystr[0]
+let count = 0;
+
+    for (i = 0; i <= arrystr.length; i++) {
+
+        if (tempData == sortarrystr[i]) {
+            count++;
+        } else {
+            console.log(`element ${tempData} count ${count}`);
+            tempData = sortarrystr[i];
+            count =1;
+        }
+    }
+
